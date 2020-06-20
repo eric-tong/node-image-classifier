7a3018401a58eefa0639391fab599e2c364b39cf
eric-tong <erictong.designs@gmail.com>
2020-06-20 15:40:51 +0800

    [Setup] Add tfjs

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,24 @@
-console.log("Hello World!");
+import fs from "fs";
+import { promisify } from "util";
+
+import tf = require("@tensorflow/tfjs-node");
+import mobileNet = require("@tensorflow-models/mobilenet");
+
+console.log("Version", tf.version.tfjs);
+const readFile = promisify(fs.readFile);
+main();
+
+async function main() {
+  const imageArray = await getArrayFromImage("./dataset/cat.jpg");
+  const imageTensor = tf.node.decodePng(imageArray);
+
+  const net = await mobileNet.load();
+  const result = await net.classify(imageTensor);
+  console.log(result);
+}
+
+async function getArrayFromImage(path: string) {
+  const image = await readFile(path);
+  const buf = Buffer.from(image);
+  return new Uint8Array(buf);
+}
