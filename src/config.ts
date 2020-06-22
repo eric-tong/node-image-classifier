cb710d5c584c4478c6849467117bbc6794926597
eric-tong <erictong.designs@gmail.com>
2020-06-22 13:54:47 +0800

    [Config] Increase training size

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,6 @@
-export const TRAINING_SIZE = 0.2;
-export const VALIDATION_SIZE = 0.01;
-export const CATEGORY_COUNT = 42 / 3;
+export const TRAINING_SIZE = 0.95;
+export const VALIDATION_SIZE = 0.05;
+export const CATEGORY_COUNT = 42;
 export const CATEGORIES = Array.from({ length: CATEGORY_COUNT }, (_, i) =>
   i.toString().padStart(2, "0")
 );
